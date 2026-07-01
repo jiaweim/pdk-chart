ea1682d3fc093387db040b6916de4ac7d96f479b
JiaweiMao <jiaweimao2019@outlook.com>
2026-07-01 13:06:33 +0800

    fix a bug in test

--- src/test/java/pdk/chart/annotations/CategoryLineAnnotationTest.java
+++ src/test/java/pdk/chart/annotations/CategoryLineAnnotationTest.java
@@ -16,7 +16,7 @@ public class CategoryLineAnnotationTest {
     @Test
     public void testConstructorExceptions() {
         Stroke stroke = new BasicStroke(2.0f);
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(NullPointerException.class, () -> {
             CategoryLineAnnotation a1 = new CategoryLineAnnotation(null, 20.0, "Cat2", 200.0,
                     Color.BLUE, stroke);
         });
@@ -24,7 +24,7 @@ public class CategoryLineAnnotationTest {
             CategoryLineAnnotation a1 = new CategoryLineAnnotation("Cat1", Double.NaN, "Cat2", 200.0,
                     Color.BLUE, stroke);
         });
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(NullPointerException.class, () -> {
             CategoryLineAnnotation a1 = new CategoryLineAnnotation("Cat1", 20.0, null, 200.0,
                     Color.BLUE, stroke);
         });
@@ -32,11 +32,11 @@ public class CategoryLineAnnotationTest {
             CategoryLineAnnotation a1 = new CategoryLineAnnotation("Cat1", 20.0, "Cat2", Double.NaN,
                     Color.BLUE, stroke);
         });
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(NullPointerException.class, () -> {
             CategoryLineAnnotation a1 = new CategoryLineAnnotation("Cat1", 20.0, "Cat2", 200.0,
                     null, stroke);
         });
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(NullPointerException.class, () -> {
             CategoryLineAnnotation a1 = new CategoryLineAnnotation("Cat1", 20.0, "Cat2", 200.0,
                     Color.BLUE, null);
         });
